011ff25298cfa1415b8c7ad94892841caa9aa119
Federico Grimaldi <federico.grimaldi98@gmail.com>
2024-05-28 10:15:06 +0200

    C/E traverses

--- REPTILE/CoverE.py
+++ REPTILE/CoverE.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 
-from REPTILE import Computable
-from REPTILE import Calculated
+from REPTILE import Computable, SpectralIndex, Traverse
+from REPTILE import Calculated, CalculatedSpectralIndex, CalculatedTraverse
 from REPTILE.utils import ratio_v_u, _make_df
 
 
@@ -20,8 +20,21 @@ class CoverE:
         self._check_consistency()
 
     def _check_consistency(self) -> None:
-        if not self.c.deposit_ids == self.e.deposit_ids:
-            raise Exception("Inconsistent deposits between C and E.")
+        self._check_class_consistency()
+        if isinstance(self.e, Traverse):
+            if not self.c.deposit_id == self.e.deposit_id:
+                raise Exception("Inconsistent deposits between C and E.")
+        if isinstance(self.e, SpectralIndex):
+            if not self.c.deposit_ids == self.e.deposit_ids:
+                raise Exception("Inconsistent deposits between C and E.")
+        
+    def _check_class_consistency(self):
+        if ((isinstance(self.c, CalculatedTraverse) and not isinstance(self.e, Traverse)) or
+            (isinstance(self.e, Traverse) and not isinstance(self.c, CalculatedTraverse))):
+            raise Exception("Cannot compare Traverse and non-Traverse object.")
+        if (isinstance(self.c, CalculatedSpectralIndex) and not isinstance(self.e, SpectralIndex) or
+            isinstance(self.e, SpectralIndex) and not isinstance(self.c, CalculatedSpectralIndex)):
+            raise Exception("Cannot compare SpectralIndex and non-SpectralIndex object.")
 
     @property
     def deposit_ids(self) -> list[str]:
@@ -35,6 +48,20 @@ class CoverE:
             A list containing the deposit IDs of the numerator and denominator.
         """
         return self.c.deposit_ids
+
+    def _compute_si(self, *args, **kwargs):
+        v, u = ratio_v_u(self.c.calculate(), self.e.compute(*args, **kwargs))
+        return _make_df(v, u)
+
+    def _compute_traverse(self, *args, **kwargs):
+        exp = self.e.compute(*args, **kwargs)
+        cal = self.c.calculate()
+        out = []
+        for t in exp.traverse:
+            v, u = ratio_v_u(cal.query("traverse == @t"),
+                             exp.query("traverse == @t"))
+            out.append(_make_df(v.iloc[0], u.iloc[0]).assign(traverse=t))
+        return pd.concat(out, ignore_index=True)
 
     def compute(self, *args, **kwargs) -> pd.DataFrame:
         """
@@ -63,8 +90,11 @@ class CoverE:
            value  uncertainty
         0    ...          ...
         """
-        v, u = ratio_v_u(self.c.calculate(), self.e.compute(*args, **kwargs))
-        return _make_df(v, u)
+        if isinstance(self.e, SpectralIndex):
+            out = self._compute_si(*args, **kwargs)
+        if isinstance(self.e, Traverse):
+            out = self._compute_traverse(*args, **kwargs)
+        return out
 
     def minus_one_percent(self, *args, **kwargs):
         """
--- tests/test_CoverE.py
+++ tests/test_CoverE.py
@@ -1,12 +1,14 @@
 import pytest
-from REPTILE.Computables import NormalizedFissionFragmentSpectrum, SpectralIndex
-from REPTILE.FissionFragmentSpectrum import FissionFragmentSpectrum, FissionFragmentSpectra
+from REPTILE.Computables import NormalizedFissionFragmentSpectrum, SpectralIndex, Traverse
+from REPTILE.FissionFragmentSpectrum import FissionFragmentSpectrum
 from REPTILE.EffectiveMass import EffectiveMass
 from REPTILE.ReactionRate import ReactionRate
 from REPTILE.CoverE import CoverE
-from REPTILE.Calculated import CalculatedSpectralIndex
-from datetime import datetime
+from REPTILE.Calculated import CalculatedSpectralIndex, CalculatedTraverse
+from datetime import datetime, timedelta
 import pandas as pd
+import numpy as np
+
 
 @pytest.fixture
 def sample_spectrum_data():
@@ -81,22 +83,116 @@ def sample_c(sample_c_si_data):
     return CalculatedSpectralIndex(sample_c_si_data, 'M', ['D1', 'D2'])
 
 @pytest.fixture
-def sample_ce(sample_c, sample_spectral_index):
+def sample_si_ce(sample_c, sample_spectral_index):
     return CoverE(sample_c, sample_spectral_index)
 
-def test_deposit_ids(sample_ce):
-    assert sample_ce.deposit_ids == ['D1', 'D2']
+counts = [0,0,0,0,0,.3,.3,.4,.1,.2,.5,0,.0,1,1,1.5,2,2.5,2,3,3.5,4,4.2,3.8,4.2,3.9,3.9,4.2,4.1,4,
+          0,0,0,0,0,.3,.3,.4,.1,.2,.5,0,.0,1,1,1.5,2,2.5,2,3,3.5,4,4.2,3.8,4.2,3.9,3.9,4.2,4.1,4,
+          0,0,0,0,0,.3,.3,.4,.1,.2,.5,0,.0,1,1,1.5,2,2.5,2,3,3.5,4,4.2,3.8,4.2,3.9,3.9,4.2,4.1,4,
+          3700,4000,4100,4200,3800,3700,3800,3900,4200,
+          3900,3900,4200,4100,4000,3700,4000,4100,4200,3800,3700,3800,3900,4200,3900,3900,4200,4100,
+          4000,3700,4000,4100,4200,3800,
+          3900,3900,4200,4100,4000,3700,4000,4100,4200,3800,3700,3800,3900,4200,3900,3900,4200,4100,
+          4000,3700,4000,4100,4200,3800, 3800,3700,3800,3900,4200,
+          3900,3900,4200,4100,4000,3700, 3800,3700,3800,3900,4200,
+          3900,3900,4200,4100,4000,3700,
+          3900,3900,4200,4100,4000,3700,4000,4100,4200,3800,3700,3800,3900,4200,3900,3900,4200,4100,
+          4000,3700,4000,4100,4200,3800,3800,3700,3800,3900,4200,
+          3900,3900,4200,4100,4000,3700,
+          3900,3900,4200,4100,4000,3700,4000,4100,4200,3800,3700,3800,3900,4200,3900,3900,4200,4100,
+          4000,3700,4000,4100,4200,3800,3800,3700,3800,3900,4200,
+          3900,3900,4200,4100,4000,3700,
+          4200,3800, 3900,3900,4200,4100,4000,3700, 4200,3800, 3900,3900,4200,4100,4000,3700, 4200,3800, 3900,3900,4200,4100,4000,3700,
+          4200,3800, 3900,3900,4200,4100,3800,3700,3800,3900,4200,
+          3900,3900,4200,4100,4000,3700,
+          4000,3700, 4200,3800, 3900,3900,4200,4100,4000,3700, 4200,3800, 3900,3900,4200,4100,4000,3700,
+          4200,3800, 3900,3900,4200,4100,4000,3700, 4200,3800, 3900,3900,4200,4100,4000,3700, 4200,3800, 3900,3900,4200,4100,4000,3700,
+          4100,4000,3700,4000,4100,4200,3800,3700,3800,3900,4200,3900,3900,4200,4100,
+          4000,3700,4000,4100,4200,3800, 4200,3800, 3900,3900,4200,4100,4000,3700, 4200,3800, 3900,3900,4200,4100,4000,3700, 4200,3800,
+          3900,3900,4200,4100,4000,3700, 4100,4000,3700,4000,4100,4200,3800,3700,3800,3900,4200,3900,3900,4200,4100,
+          4000,3700,4000,4100,4200,3800, 4100,4000,3700,4000,4100,4200,3800,3700,3800,3900,4200,3900,3900,4200,4100,
+          4000,3700,4000,4100,4200,3800,
+          4200,3800, 3900,3900,4200,4100,4000,3700, 4200,3800, 3900,3900,4200,4100,4000,3700, 4200,3800, 3900,3900,4200,4100,4000,3700,
+          3.7,3.8,3.9,4,4,4.2,4.1,3.5,3.2,3,2.5,2.2,2,2,1.5,1.5,1.6,1,1,1,
+          .5,.6,.4,.3,.5,.3,.5,.6,.1,.3,.2,.1,0,0,0,0,0,0,0,
+          0,0,0,0,0,.3,.3,.4,.1,.2,.5,0,.0,1,1,1.5,2,2.5,2,3,3.5,4,4.2,3.8,4.2,3.9,3.9,4.2,4.1,4,
+          0,0,0,0,0,.3,.3,.4,.1,.2,.5,0,.0,1,1,1.5,2,2.5,2,3,3.5,4,4.2,3.8,4.2,3.9,3.9,4.2,4.1,4,
+          3.7,3.8,3.9,4,4,4.2,4.1,3.5,3.2,3,2.5,2.2,2,2,1.5,1.5,1.6,1,1,1,
+          .5,.6,.4,.3,.5,.3,.5,.6,.1,.3,.2,.1,0,0,0,0,0,0,0,
+          3.7,3.8,3.9,4,4,4.2,4.1,3.5,3.2,3,2.5,2.2,2,2,1.5,1.5,1.6,1,1,1,
+          .5,.6,.4,.3,.5,.3,.5,.6,.1,.3,.2,.1,0,0,0,0,0,0,0,]
 
-def test_compute(sample_ce):
+@pytest.fixture
+def rr1():
+    time = [datetime(2024,5,27,13,19,20) + timedelta(seconds=i) for i in range(len(counts))]
+    data =  pd.DataFrame({'Time': time, 'value': counts})
+    return ReactionRate(data, data.Time.min(),
+                        campaign_id='A', experiment_id='B',
+                        detector_id=1, deposit_id='dep')
+
+@pytest.fixture
+def rr2():
+    time = [datetime(2024,5,27,15,12,42) + timedelta(seconds=i) for i in range(len(counts))]
+    data = pd.DataFrame({'Time': time, 'value': np.array(counts) / 2})
+    return ReactionRate(data, data.Time.min(),
+                        campaign_id='A', experiment_id='B',
+                        detector_id=1, deposit_id='dep')
+
+@pytest.fixture
+def monitor1(rr1):
+    data_ = rr1.data.copy()
+    data_.value = data_.value.apply(lambda x: 600 if x > 1000 else 1)
+    return ReactionRate(data_, data_.Time.min(),
+                        campaign_id='A', experiment_id='B',
+                        detector_id=2, deposit_id='dep')
+
+@pytest.fixture
+def monitor2(rr2):
+    data_ = rr2.data.copy()
+    data_.value = data_.value.apply(lambda x: 600 if x > 1000 else 1)
+    return ReactionRate(data_, data_.Time.min(),
+                        campaign_id='A', experiment_id='B',
+                        detector_id=2, deposit_id='dep')
+
+@pytest.fixture
+def sample_traverse_rr(rr1, rr2):
+    return Traverse({'loc A': rr1, 'loc B': rr2})
+
+@pytest.fixture
+def sample_c_traverse_data():
+    return pd.DataFrame({'value': [1.01, 0.5],
+                         'uncertainty': [0.01, 0.01],
+                         'traverse': ['loc A', 'loc B']})
+
+@pytest.fixture
+def sample_c_traverse(sample_c_traverse_data):
+    return CalculatedTraverse(sample_c_traverse_data, 'M', 'dep')
+
+@pytest.fixture
+def sample_ce_traverse(sample_c_traverse, sample_traverse_rr):
+    return CoverE(sample_c_traverse, sample_traverse_rr)
+
+def test_deposit_ids(sample_si_ce):
+    assert sample_si_ce.deposit_ids == ['D1', 'D2']
+
+def test_compute_si(sample_si_ce):
     expected_df = pd.DataFrame({'value': 1.01,
                                 'uncertainty': 0.08323682675073317,
                                 'uncertainty [%]': 8.241269975320115},
                                 index=['value'])
-    pd.testing.assert_frame_equal(expected_df, sample_ce.compute(), check_exact=False, atol=0.00001)
+    pd.testing.assert_frame_equal(expected_df, sample_si_ce.compute(), check_exact=False, atol=0.00001)
 
-def test_minus_one_per_cent(sample_ce):
+def test_compute_traverse(sample_ce_traverse, monitor1, monitor2):
+    expected_df = pd.DataFrame({'value': [1.01, 1.  ],
+                                'uncertainty': [0.01055819, 0.02029745],
+                                'uncertainty [%]': [1.04536498, 2.02974473],
+                                'traverse': ['loc A', 'loc B']})
+    pd.testing.assert_frame_equal(expected_df, sample_ce_traverse.compute([monitor1, monitor2]))
+
+def test_minus_one_per_cent(sample_si_ce):
     expected_df = pd.DataFrame({'value': 1.,
                                 'uncertainty': 8.323682675073316,
                                 'uncertainty [%]': 832.3682675073309},
                                 index=['value'])
-    pd.testing.assert_frame_equal(expected_df, sample_ce.minus_one_percent(), check_exact=False, atol=0.00001)
+    pd.testing.assert_frame_equal(expected_df, sample_si_ce.minus_one_percent(), check_exact=False, atol=0.00001)
+
