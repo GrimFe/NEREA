fd11680913f3fa04d388f6e0a2fa582f6edf0676
Federico Grimaldi <federico.grimaldi98@gmail.com>
2024-05-28 08:44:08 +0200

    ComputedTraverse

--- REPTILE/Calculated.py
+++ REPTILE/Calculated.py
@@ -35,12 +35,14 @@ class CalculatedSpectralIndex(Calculated):
 
         Returns
         -------
-        C
-            An instance of the `C` class created from the specified file.
+        CalculatedSpectralIndex
+            An instance of the `CalculatedSpectralIndex` class created
+            from the specified file.
 
         Examples
         --------
-        >>> c_instance = C.from_sts('file.det', 'SI_detector', model_id='Model1')
+        >>> c_instance = CalculatedSpectralIndex.from_sts('file.det',
+                                                    'SI_detector', model_id='Model1')
         """
         ## works with relative uncertainties for the moment.
         ## Shall be homogenized with the rest of the API
@@ -63,12 +65,14 @@ class CalculatedSpectralIndex(Calculated):
 
         Returns
         -------
-        C
-            An instance of the `C` class created from the specified file.
+        CalculatedSpectralIndex
+            An instance of the `CalculatedSpectralIndex` class created from
+            the specified file.
 
         Examples
         --------
-        >>> c_instance = C.from_sts_detectors('file.det', ['detector1', 'detector2'], model_id='Model1')
+        >>> c_instance = CalculatedSpectralIndex.from_sts_detectors('file.det', 
+                                                    ['detector1', 'detector2'], model_id='Model1')
         """
         v1, u1 = sts.read(file).detectors[detector_names[0]].bins[0][-2:]
         v2, u2 = sts.read(file).detectors[detector_names[1]].bins[0][-2:]
@@ -78,18 +82,63 @@ class CalculatedSpectralIndex(Calculated):
 
     def calculate(self):
         """
-        Computes the cover-e value. Alias for self.data.
+        Computes the C value. Alias for self.data.
 
         Returns
         -------
         pd.DataFrame
-            DataFrame containing the cover-e value.
+            DataFrame containing the C value.
 
         Examples
         --------
-        >>> c_instance = C(data=pd.DataFrame({'value': [0.5]}), model_id='Model1', deposit_ids='Dep1')
+        >>> c_instance = CalculatedSpectralIndex(data=pd.DataFrame({'value': [0.5]}),
+                            model_id='Model1', deposit_ids='Dep1')
         >>> c_instance.compute()
            value
         0    0.5
         """
         return self.data
+
+@dataclass(slots=True)
+class CalculatedTraverse:
+    data: pd.DataFrame
+    model_id: str
+    deposit_id: str
+
+    @classmethod
+    def from_sts(cls, file: str, detector_names: list[str], **kwargs):
+        """
+        Creates an instance using data extracted from a Serpent det.m
+        file for multiple detectors.
+
+        Parameters
+        ----------
+        file : str
+            The file path from which data will be read.
+        detector_names : Iterable[str]
+            The names of the detectors from which data will be extracted.
+
+        Returns
+        -------
+        CalculatedTraverse
+            An instance of the `CalculatedTraverse` class created from
+            the specified file.
+        """
+        out = []
+        for d in detector_names:
+            v, u = sts.read(file).detectors[detector_names].bins[0][-2:]
+            out.append(_make_df(v, u).assign(traverse=d))
+        return cls(data=pd.concat(out, ignore_index=True), **kwargs)
+
+    def calculate(self):
+        """
+        Computes the C value. Alias for self.data.
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame containing the C value.
+
+        """
+        return self.data
+    
