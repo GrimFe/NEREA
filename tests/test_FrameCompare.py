858a6accb33d261c84a3ece975b8dc496074bff7
Federico Grimaldi <federico.grimaldi98@gmail.com>
2024-08-13 14:11:19 +0200

    DataFrame comparison

---
+++ tests/test_FrameCompare.py
@@ -0,0 +1,34 @@
+import pytest
+import pandas as pd
+from REPTILE.Comparisons import FrameCompare
+
+@pytest.fixture
+def frame1():
+    return pd.DataFrame({'value': 1.01, 'uncertainty': 0.03,
+                         'uncertainty [%]': 2.9702970297029703,
+                         'VAR_C_n': None, 'VAR_C_d': None,
+                         'VAR_C': 0.0009}, index=['value'])
+
+@pytest.fixture
+def frame2():
+    return pd.DataFrame({'value': 1, 'uncertainty': 0.02,
+                         'uncertainty [%]': 2,
+                         'VAR_C_n': None, 'VAR_C_d': None,
+                         'VAR_C': 0.0004}, index=['value'])
+
+@pytest.fixture
+def sample_fc(frame1, frame2):
+    return FrameCompare(frame1, frame2)
+
+def test_fc(sample_fc):
+    expected_df = pd.DataFrame({'value': 1.01,
+                                'uncertainty': 0.036167,
+                                'uncertainty [%]': 3.580875,
+                                'VAR_C_n_n': None,
+                                'VAR_C_d_n': None,
+                                'VAR_C_n': 0.0009,
+                                'VAR_C_n_d': None,
+                                'VAR_C_d_d': None,
+                                'VAR_C_d': 0.00040804}, index=['value'])
+
+    pd.testing.assert_frame_equal(expected_df, sample_fc.compute(), check_exact=False, atol=0.00001)
