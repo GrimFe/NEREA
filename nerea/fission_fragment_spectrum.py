d3f4a755cf3b3102df469dedb83a54d729945b54
GrimFe <69858810+GrimFe@users.noreply.github.com>
2025-03-05 08:25:20 +0100

    Update fission_fragment_spectrum.py (#32)
    typing_extensions -> typing as we now use python 3.11

--- nerea/fission_fragment_spectrum.py
+++ nerea/fission_fragment_spectrum.py
@@ -1,6 +1,6 @@
 from collections.abc import Iterable
 from dataclasses import dataclass
-from typing_extensions import Self
+from typing import Self
 import pandas as pd
 import numpy as np
 import warnings
